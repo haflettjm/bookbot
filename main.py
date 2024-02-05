4593b369bf6a4ddc9ac88580949f5935ce865280
haflettjm <hafletjm01@gmail.com>
2024-02-05 00:45:46 -0500

    added a letter counter for all the letters in the book

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import string
 
 def readBookText(path):
     with open(path, 'r') as f:
@@ -9,9 +9,34 @@ def printLotsOText(text):
     for line in text:
         print(line, end='')
 
+def letterCounter(someString, letterCount):
+    splitString = list(someString.lower())
+    alphabet = string.ascii_lowercase
+    for letter in alphabet:
+        for char in splitString:
+            if char == letter:
+                if letter in letterCount:
+                    letterCount[letter] += 1
+                else:
+                    letterCount[letter] = 1
+        if letter not in letterCount:
+            letterCount[letter] = 0
+    return letterCount
+
+
+def countWords(text):
+    words = text.split()
+    return len(words)
 def main():
     book = "./books/frankenstein.txt"
     data = readBookText(book)
-    printLotsOText(data)
+    letterCount = {}
+    totalWords = countWords(data)
+    letterCount = letterCounter(data, letterCount)
+
+    for letter in letterCount:
+        print(f"The count of this {letter}:\n {letterCount[letter]}\n")
+    print(f"The length of letterCount is {len(letterCount)}")
+    # printLotsOText(data)
 
 main()
